d3cb0540c4c219df4b84414bb070e4d6d2393c0e
LinnaIkae <leevi.i.karjalainen@gmail.com>
2019-07-17 22:19:06 +0300

    Motor start working fine with pwmWrite function.

--- src/main.1.cpp
+++ src/main.1.cpp
@@ -1,46 +1,45 @@
 #include "Arduino.h"
 #include "HardwareTimer.h"
-#define MOTOR PA1
+#define MOTOR PA8
 
 HardwareTimer timer(1);
-
-void handler_motor_up(void){
-    digitalWrite(MOTOR, HIGH);
-}
-
-void handler_motor_down(void){
-    digitalWrite(MOTOR, LOW);
-}
 
 void setup()
 {
     Serial1.begin(9600);
     Serial1.println("Hello there");
-    pinMode(PA1, OUTPUT);
-    // Desired frequency = 500 Hz
+    pinMode(MOTOR, PWM);
+    // Frame time = 20 ms
+    // On time 1-2 ms
     // SysClk frequency = 72 MHz
-    // Prescaler = 72 => Scaled frequency = 1 MHz
-    // Overflow = 2000
-    // Overflowing frequency = 500 Hz.
+    // Overflow = 65535 (default)
+    // Prescaler = 21 => Scaled input frequency = ~3,43 MHz
+    // Overflow frequency = 3,43.. MHz / 65535 = 52,3166 Hz
+    // Period = 19 ms
     timer.pause();
-    timer.setPrescaleFactor(72);
-    timer.setOverflow(2000);
-    timer.setMode(TIMER_CH1, TIMER_OUTPUT_COMPARE);
-    timer.setCompare(TIMER_CH1, 0);
-    timer.setMode(TIMER_CH2, TIMER_OUTPUT_COMPARE);
-    timer.setCompare(TIMER_CH2, 1000); // Set to 1 ms pulse initially.
-    timer.attachInterrupt(TIMER_CH1, handler_motor_up);
-    timer.attachInterrupt(TIMER_CH2, handler_motor_down);
+    timer.setPrescaleFactor(21);
     timer.refresh();
     timer.resume();
+
+    Serial1.println("Starting pwm on 1ms duty cycle");
+    pwmWrite(MOTOR, 3449);
+    delay(10000);
+
 }
 
 void loop()
 {
-    if(Serial1.available() > 0){
-        int newCompare = Serial1.read();
-        Serial1.println(newCompare);
-        timer.setCompare(TIMER_CH2, newCompare);
-        delay(500);
+    Serial1.println("PWM cycling through lowest to highest");
+    for(int duty=3549; duty < 6898; duty+=100){    
+        Serial1.print("Writing new duty: ");
+        Serial1.println(duty);
+        pwmWrite(MOTOR, duty);
+        // duty% = newDuty/65535 * 100%
+        // duty ms = newDuty/65535 * 19 ms
+        // duty 1 ms:
+        //   newDuty = (1 ms * 65535) / 19 ms = 3449
+        // duty 2 ms:
+        //   newDuty = 6898
+        delay(2000);
     }
 }
